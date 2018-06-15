22c4a0989336b056409229d8c0979ee75ac4ad7f
Eric Guyader <eric@MacBook-Pro-de-Eric.local>
2018-06-15 11:02:55 +0200

    Ajout mode affichage

--- src/views/StatsMensuelles.vue
+++ src/views/StatsMensuelles.vue
@@ -7,7 +7,16 @@
             <label for="rech-projet" class="sr-only">Projet:</label> <b-input type="text" placeholder="Projet" id="rech-projet" v-model="recherche.projet" class="col-3"></b-input>
             <b-button @click="getTimesFromServer" variant="primary" class="col-1">OK</b-button>
         </b-form>
-        <temps-passes :temps="resultats" :titre="'Temps passés ' + recherche.mois"></temps-passes>
+        <b-select v-if="resultats.length > 0" v-model="displayMode">
+            <option value="">Standard</option> 
+            <option value="client">Par client</option> 
+            <option value="clientFinal">Par client final</option> 
+            <option value="projet">Par projet</option> 
+        </b-select>
+        <temps-passes v-if="displayMode === ''" :temps="resultats" :titre="'Temps passés ' + recherche.mois"></temps-passes>
+        <div v-else>
+            <temps-passes v-for="(unite,index) in resultatsRanges" :temps="unite.temps" :titre="unite.nom" :key="index"></temps-passes>
+        </div>
     </div>
 </template>
 
@@ -20,6 +29,7 @@ export default {
         return {
             recherche:{},
             resultats:[],
+            displayMode:''
         }
     },
     methods:{
@@ -38,9 +48,6 @@ export default {
             if(undefined !== this.recherche.projet && this.recherche.projet !== ''){
                 url += 'projet=' + this.recherche.projet + '&'
             }
-            // this.$http.get('http://localhost:3000/ts/2018-06?clientFinal=Laboratoires+Lebeau')
-            // this.$http.get('http://localhost:3000/ts/2018-06?clientFinal=Promologis')
-            // this.$http.get('http://localhost:3000/ts/2018-06?clientFinal=Fauché')
             this.$http.get(url)
             .then(response => {
                 this.resultats = response.data;
@@ -49,6 +56,28 @@ export default {
     },
     components:{
         'temps-passes':TempsPasses
+    },
+    computed:{
+        resultatsRanges(){
+            let res = [];
+            this.resultats.forEach(temps => {
+                let intitule = '(Aucun)'
+                if( temps[this.displayMode] !== undefined ) {
+                    intitule = temps[this.displayMode]
+                }
+                let resItem = res.find( item => item.nom === intitule  )
+                
+                
+                if(resItem){
+                    resItem.temps.push(temps)
+                }else{
+                    res.push({nom:intitule,temps:[]})
+                    res[res.length-1].temps.push(temps)
+                }
+            });
+            res.sort((a,b) => a.nom > b.nom )
+            return res;
+        }
     }   
 }
 </script>
